3dbc5f1e476978cabf490ced04fe05dc53fd7cc3
Gary <garyherd@yahoo.com>
2015-04-07 00:10:30 -0500

    Add first draft of python script

---
+++ pjm_lmp.py
@@ -0,0 +1,42 @@
+import json
+import requests
+import sqlite3
+
+data = {"startDate": "2015-04-01", "endDate": "2015-04-01", "pnodeList": [5021072]}
+data = json.dumps(data)
+
+url = 'https://dataminer.pjm.com/dataminer/rest/public/api/markets/realtime/lmp/daily'
+
+headers = {'Content-Type': 'application/json'}
+
+response = requests.post(url, data=data, headers=headers)
+
+print(response.status_code)
+result_json = response.json()
+print (result_json)
+
+# Create a table with same headers as the CSV export from the GUI-based DataMiner
+with sqlite3.connect("pjm_lmp.db") as connection:
+    c = connection.cursor()
+    c.executescript("""
+    DROP TABLE IF EXISTS LMP;
+    CREATE TABLE LMP(PublishDate TEXT, Version TEXT,
+                        PnodeID TEXT, PricingType TEXT,
+                        H1 REAL, H2 REAL, H3 REAL, H4 REAL,
+                        H5 REAL, H6 REAL, H7 REAL, H8 REAL,
+                        H9 REAL, H10 REAL, H11 REAL, H12 REAL,
+                        H13 REAL, H14 REAL, H15 REAL, H16 REAL,
+                        H17 REAL, H18 REAL, H19 REAL, H20 REAL,
+                        H21 REAL, H22 REAL, H23 REAL, H24 REAL);
+    """)
+
+json_transform = {}
+
+for record in result_json:
+    json_transform['pnodeId'] = record['pnodeId']
+    print(record['pnodeId'], " ", record['priceType'], " ", end="")
+    for price in record['prices']:
+        # print('utchour: ', price['utchour'],end=""),
+        print(price['price'], " ", end=""),
+    print("")
+
